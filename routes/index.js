7e9629c0fc5b2748decd63fc6af1fdc05dad942b
Tomer Buzaglo <tomerbuzaglo@Tomers-iMac.local>
2016-02-11 10:44:08 +0200

    added some routes, using the error middleware simillar to flash

--- routes/index.js
+++ routes/index.js
@@ -8,5 +8,37 @@ module.exports = function(passport){
 	  res.render('index', { title: 'Express' });
 	});
 
+	router.get('/login', (req, res)=>{
+		res.render('login', {message:'login'});
+	});
+
+	router.get('/signup', (req, res)=>{
+		res.render('login', {message:'signup'});
+	});
+
+
+	// route middleware to make sure a user is logged in
+	function isLoggedIn(req, res, next) {
+	    // if user is authenticated in the session, carry on 
+	    if (req.isAuthenticated())
+	        return next();
+	    // if they aren't redirect them to the home page
+	    req.session.error = 'Not Logged In!, can\'t see profile'
+	    res.redirect('/');
+	}
+	//protected route using the isLoggedIn middleware
+    router.get('/profile', isLoggedIn, function(req, res) {
+        res.render('profile', {
+            user : req.user // get the user out of session and pass to template
+        });
+    });
+
+    router.get('/logout', function(req, res) {
+    	req.session.notice = 'Logged you out';
+        req.logout();
+        res.redirect('/');
+    });
+
+
 	return router;
 }
